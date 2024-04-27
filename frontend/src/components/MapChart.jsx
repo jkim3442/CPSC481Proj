8795d8c785af67193227879038eeafa995da4b44
thecodeofdaniel <thecodeofdaniel+github@gmail.com>
2024-04-27 15:08:51 -0700

    Use obj to map state to value including capital and coords

--- frontend/src/components/MapChart.jsx
+++ frontend/src/components/MapChart.jsx
@@ -8,33 +8,30 @@ import {
   ZoomableGroup,
 } from 'react-simple-maps';
 
-const markers = [
-  {
-    markerOffset: -10,
-    name: 'Sacramento',
+const markers = {
+  CA: {
+    capital: 'Sacramento',
     coordinates: [-121.493629, 38.576668],
   },
-  {
-    markerOffset: -20,
-    name: 'Tallahassee',
+  FL: {
+    capital: 'Tallahassee',
     coordinates: [-84.281296, 30.438118],
   },
-  {
-    markerOffset: -20,
-    name: 'Honolulu',
+  HI: {
+    capital: 'Honolulu',
     coordinates: [-157.857376, 21.307442],
   },
-  {
-    markerOffset: -20,
-    name: 'Austin',
-    coordinates: [-97.740349, 30.27467],
+  NY: {
+    capital: 'Albany',
+    coordinates: [-73.757874, 42.652843],
   },
-];
+};
 
 const geoUrl = '/states.json';
 
 function MapChart() {
   console.log('- <MapChart>');
+  const [statePicked, setStatePicked] = useState(undefined);
 
   return (
     <>
@@ -65,29 +62,36 @@ function MapChart() {
               })
             }
           </Geographies>
-          {markers.map(({ name, coordinates, markerOffset }) => (
-            <Marker key={name} coordinates={coordinates} id={name}>
+
+          {Object.entries(markers).map(([state, value]) => (
+            <Marker key={state} coordinates={value.coordinates}>
               <circle r={5} fill="#E42A1D" stroke="#fff" strokeWidth={2} />
               <text
                 textAnchor="middle"
-                y={markerOffset}
+                y={-10}
                 style={{ fontFamily: 'system-ui', fill: '#5D5A6D' }}
               >
-                {name}
+                {value.capital}
               </text>
             </Marker>
           ))}
           <Line
-            from={[-121.493629, 38.576668]}
-            to={[-97.740349, 30.27467]}
-            strokeLinecap="round"
+            stroke="#FF5533"
+            coordinates={[markers.CA.coordinates, markers.FL.coordinates]}
+          />
+          <Line
+            from={markers.CA.coordinates}
+            to={markers.NY.coordinates}
             stroke="#FF5533"
           />
           <Line
-            from={[-121.493629, 38.576668]}
-            to={[-157.857376, 21.307442]}
-            strokeLinecap="round"
+            from={markers.CA.coordinates}
+            to={markers.HI.coordinates}
             stroke="#FF5533"
+          />
+          <Line
+            stroke="#005533"
+            coordinates={[markers.FL.coordinates, markers.NY.coordinates]}
           />
         </ZoomableGroup>
       </ComposableMap>
